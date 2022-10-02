dd1b9a9a454670031880d8501c0ebbaf25e68275
criticalAY <mailtoashish693@gmail.com>
2022-10-03 02:41:55 +0530

    Update MainActivity.kt
    final commit

--- app/src/main/java/com/pratyakshkhurana/quizapp/MainActivity.kt
+++ app/src/main/java/com/pratyakshkhurana/quizapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var pressedTime: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
